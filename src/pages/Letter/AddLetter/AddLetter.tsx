a0ee1365f132d61123cbf66caf11da662b6ac8c8
Stanlys <3995782+stanlys@users.noreply.github.com>
2023-01-22 15:49:02 +0300

    feat: add yup to form addtrack number

--- src/pages/Letter/AddLetter/AddLetter.tsx
+++ src/pages/Letter/AddLetter/AddLetter.tsx
@@ -1,20 +1,52 @@
-import { Box, Divider, Paper, TextField, Typography } from "@mui/material";
-import { useFormik } from "formik";
+import {
+  Box,
+  Button,
+  Divider,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useFormik, yupToFormErrors } from "formik";
 import style from "./AddLetter.module.scss";
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 import React from "react";
 import { DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import dayjs, { Dayjs } from "dayjs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import TransferWithinAStationIcon from "@mui/icons-material/TransferWithinAStation";
+import BusinessIcon from "@mui/icons-material/Business";
+import Person2Icon from "@mui/icons-material/Person2";
+import ShareLocationIcon from "@mui/icons-material/ShareLocation";
 import "dayjs/locale/ru";
+import * as Yup from "yup";
+
+import { string } from "prop-types";
 
 const AddLetter = () => {
+  const validateYup = Yup.object({
+    trackNumber: Yup.string()
+      .min(6, "Должно быть длиннее 6 символов")
+      .required("Обязательное поле"),
+    receiver: Yup.string()
+      .min(6, "Должно быть длиннее 6 символов")
+      .required("Обязательное поле"),
+    address: Yup.string()
+      .min(6, "Должно быть длиннее 6 символов")
+      .required("Обязательное поле"),
+    sender: Yup.string()
+      .min(6, "Должно быть длиннее 6 символов")
+      .required("Обязательное поле"),
+  });
+
   const formAddPost = useFormik({
     initialValues: {
-      firstName: "",
-      lastName: "",
-      email: "",
+      date: Date.now(),
+      trackNumber: "",
+      receiver: "",
+      address: "",
+      sender: "",
     },
+    validationSchema: validateYup,
     onSubmit: (values) => {
       console.log(values);
     },
@@ -27,36 +59,62 @@ const AddLetter = () => {
   };
 
   return (
-    <Box>
+    <Box component={"form"} onSubmit={formAddPost.submitForm}>
       <Typography variant="h5"> Добавление нового отправления</Typography>
       <Divider sx={{ mt: 2 }} />
 
       <Paper elevation={2} className={style.form}>
         <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={"ru"}>
-          <DesktopDatePicker
-            label="Date desktop"
-            inputFormat="DD.MM.YYYY"
-            value={date}
-            onChange={handleChange}
-            renderInput={(params) => <TextField {...params} />}
-          />
+          <Box className={style.datePickerGroup}>
+            <DesktopDatePicker
+              label="Дата отправки"
+              inputFormat="DD.MM.YYYY"
+              value={formAddPost.values.date}
+              onChange={formAddPost.handleChange}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  sx={{ width: 250 }}
+                  helperText={formAddPost.errors.date}
+                  error={
+                    (formAddPost.touched.date &&
+                      formAddPost.errors.date) as boolean
+                  }
+                />
+              )}
+            />
+            <Button onClick={() => setDate(dayjs(Date.now()).add(-1, "day"))}>
+              Вчера
+            </Button>
+          </Box>
         </LocalizationProvider>
         <Box sx={{ display: "flex", alignItems: "flex-end" }}>
-          <LocalShippingIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
-          <TextField label="Введите трек номер" variant="standard" fullWidth />
+          <ShareLocationIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+          <TextField
+            label="Введите трек номер"
+            variant="standard"
+            fullWidth
+            error={formAddPost.errors.trackNumber !== undefined}
+            helperText={formAddPost.errors.trackNumber}
+          />
         </Box>
         <Box sx={{ display: "flex", alignItems: "flex-end" }}>
-          <LocalShippingIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+          <Person2Icon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
           <TextField label="Получатель" variant="standard" fullWidth />
         </Box>
         <Box sx={{ display: "flex", alignItems: "flex-end" }}>
-          <LocalShippingIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+          <BusinessIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
           <TextField label="Адрес получателя" variant="standard" fullWidth />
         </Box>
         <Box sx={{ display: "flex", alignItems: "flex-end" }}>
-          <LocalShippingIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+          <TransferWithinAStationIcon
+            sx={{ color: "action.active", mr: 1, my: 0.5 }}
+          />
           <TextField label="Отправитель" variant="standard" fullWidth />
         </Box>
+        <Button type="submit" variant="outlined" color="success" >
+          Добавить
+        </Button>
       </Paper>
     </Box>
   );
