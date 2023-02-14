dfad276e45a7ffb7da65a9e36bce24c829fb56a9
Stanlys <stanlys@yandex.ru>
2023-02-14 15:21:30 +0300

    feat: install modern doc viewer

--- src/components/FileList/FileList.tsx
+++ src/components/FileList/FileList.tsx
@@ -1,7 +1,7 @@
-import { Box, List } from "@mui/material";
+import { Box, List, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { formatBytes } from "react-dropzone-uploader";
-import DocViewer, { PDFRenderer, PNGRenderer } from "react-doc-viewer";
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 import { IDocument } from "../../interfaces/letter";
 import { useAppDispatch } from "../../store/hooks";
 import { getLetter } from "../../api/letter";
@@ -15,24 +15,66 @@ const INITIAL_STATE: IDocument = {
   size: 0,
 };
 
+const MyNoRenderer = ({
+  document,
+  fileName,
+}: {
+  document: any;
+  fileName: string;
+}) => {
+  const fileText = fileName || document?.fileType || "";
+  console.log("noRender-", fileText);
+  if (fileText) {
+    return <div>No Renderer Error! ({fileText})</div>;
+  }
+
+  return <div>No Renderer Error!</div>;
+};
+
+const MyLoadingRenderer = ({ document, fileName }) => {
+  const fileText = fileName || document?.fileType || "";
+  const ext = fileText.split(".").pop();
+  if (ext === "doc" || ext === "docx")
+    return <Typography> Документ Word </Typography>;
+  console.log("loading-", fileText);
+
+  if (fileText) {
+    return <div>Loading Renderer ({fileText})...</div>;
+  }
+
+  return <div>Loading Renderer...</div>;
+};
+
 const FileList = () => {
   const a = formatBytes(10);
   const [docs, setDocs] = useState<IDocument>(INITIAL_STATE);
 
   const dispatch = useAppDispatch();
-
+  
   useEffect(() => {
     //dispatch(getLetter)
   }, []);
   const MOCK_DOCS = [
-    { uri: `${API_ENDPOINTS.BASE}/2023/2023-02-13-213759.pdf` },
+    { uri: `${API_ENDPOINTS.BASE}/2023/2023-02-13-164212.pdf` },
+    { uri: `${API_ENDPOINTS.BASE}/2023/2023-02-13-164547.pdf` },
+    { uri: `${API_ENDPOINTS.BASE}/2023/2023-02-13-164222.doc` },
+    { uri: `${API_ENDPOINTS.BASE}/2023/2023-02-13-164554.xlsx` },
   ];
   return (
     <Box>
       <List></List>
       <DocViewer
         documents={MOCK_DOCS}
-        pluginRenderers={[PDFRenderer, PNGRenderer]}
+        pluginRenderers={DocViewerRenderers}
+        config={{
+          noRenderer: {
+            overrideComponent: MyNoRenderer,
+          },
+          loadingRenderer: {
+            overrideComponent: MyLoadingRenderer,
+            showLoadingTimeout: false, // false if you want to disable or number to provide your own value (ms)
+          },
+        }}
       />
     </Box>
   );
